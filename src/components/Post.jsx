64511a5ae375c5756ebb746d227fec54272279b5
AndréBorges <borges.an@hotmail.com>
2022-10-26 21:13:28 -0300

    up Post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,12 +1,21 @@
 import { format, formatDistanceToNow } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
+import { useState } from 'react'
 
 import { Avatar } from './Avatar'
 import { Comment } from './Comment'
 
 import styles from './Post.module.css'
 
+
 export function Post({ author, publishedAt, content }) {
+    // criar um estado
+    const comments = useState([
+        1,
+        2,
+        3,
+    ])
+
     // formatar data
     const publishedDateFormatted = format(publishedAt, "dd 'de' LLLL 'às' HH:mm'h'", { locale: ptBR })
 
@@ -60,9 +69,10 @@ export function Post({ author, publishedAt, content }) {
             </form>
 
             <div className={styles.commentList}>
-                <Comment />
-                <Comment />
-                <Comment />
+                {comments.map(comment => {
+                    return <Comment />
+                }    
+                )} 
             </div>
 
         </article>
